7d602d1c577267d3d224bf9a8fe3ee48f9af916a
Audrey Couture <audrey.couture.bem@gmail.com>
2018-04-09 23:38:59 +0200

    exo 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
-#write your code here
+def translate(string)
+cons = %w[b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, z]
+voys = %w[a, e, i, o, u, y]
+
+f = []
+i = 0
+ 	lettre = string.split(" ").each do |l|
+		if voys.include?(l[0])
+			print lettre + "ay"
+		else 
+			for i in 1..3 
+				f << l[i]
+				
+			if voys.include?(l[0])
+				break
+			end
+		end
+	end
+end
+	
+	lettre << f + "ay"
+end
+
+translate("apple")
