1d5855edfc970f6774a6b8ec85e68c2b3f15e408
Audrey Couture <audrey.couture.bem@gmail.com>
2018-04-10 00:29:02 +0200

    exo 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -8,16 +8,15 @@ class Book
 private
 
   def titleize(title)
-    title_words = title.capitalize.split(" ")
-    title_words.collect { |word| cap(word) }.join(" ")
+    arr = ['a', 'an', 'the', 'over', 'and', 'in', 'of']
+  p = title.split.each {|mot| mot.capitalize! unless arr.include? (mot.downcase)}
+  n = p.length
+  if n == 1
+    p[0].capitalize
+  else
+    p[0].capitalize + " " + p[1...n].join(" ")
+  end
   end
 
-  def cap(word)
-    skip_words.include?(word) ? word : word.capitalize
-  end
-
-  def skip_words
-    [ "and", "in", "the", "of", "a", "an" ]
-  end
 
 end
